157e2a8519f2465a69d2635ac27201c855ac7ba6
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2020-12-21 22:18:43 +0300

    +edits

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dumdum</title>
+        <link href="style.css" type="text/css" rel="stylesheet">  
+    </head>
+    <body style="background-color: black;">
+         <h1 style="color: crimson;" class="top">Dumdum</h1>
+         <a href="specialreviews.html"><h3 style="color: crimson; text-align: left;">Çağan'dan Oyun Değerlendirmeleri</h3></a> <br>
+         <a href="love.html"><h3 style="color: crimson; text-align: left;">luv</h3></a>
+         <div>
+             <img src="">
+             <form action="index.html" method="post">
+                 <input type="text" placeholder="Kullanıcı Adı" class="c7" name="Kullanıcı Adı">
+                 <input type="email" placeholder="Email" class="c7" name="Email">
+                 <input type="password" placeholder="Şifre" class="c7" name="Şifre">
+                 <input type="password" placeholder="Şifre" class="c7" name="Şifreni Doğrula">
+                 <input type="button" value="Hesabı Oluştur" class="btn-save">
+             </form>
+         </div>
+    </body>
+</html>
